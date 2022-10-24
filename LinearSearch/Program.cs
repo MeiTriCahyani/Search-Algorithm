2df536a16acb9323caeeef9931ea405ba972a2c8
TB1112 <your_mei.tri.ft21@mail.umy.ac.id>
2022-10-24 16:23:03 +0700

    Add project files.

---
+++ LinearSearch/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchAlgoritm
+{
+
+    class program
+    {
+        //array to be searched
+        int[] arr = new int[20];
+
+        //number of elements in the array
+        int n;
+
+        //get the number of elements to store in the array
+        int i;
+
+        public void input()
+        {
+            while (true)
+            {
+                Console.WriteLine("enter the number of element in the array:");
+                string s = Console.ReadLine(); 
+                n = Int32.Parse(s);
+                if ((n > 0) && (n <= 20))
+                    break;
+                else
+                    Console.WriteLine("\n Array should have minimum 1 and maximum 20 element.\n");
+
+            }
+            //accept array elements
+            Console.WriteLine("");
+            Console.WriteLine("--------------------");
+            Console.WriteLine(" Enter array element");
+            Console.WriteLine("--------------------");
+            for (i = 0; i < n; i++)
+            {
+                Console.Write("<" + (i+1) + ">");
+                string s1 = Console.ReadLine();
+                arr[i] = Int32.Parse(s1);
+
+            }
+        }
+    }
+}
