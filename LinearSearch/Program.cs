9a8d0b9dc3dd9d90f8d5f42f76f8c04d0c536195
TB1112 <your_mei.tri.ft21@mail.umy.ac.id>
2022-10-24 17:19:30 +0700

    Linear search

--- LinearSearch/Program.cs
+++ LinearSearch/Program.cs
@@ -62,7 +62,7 @@ namespace SearchAlgoritm
                 int ctr = 1;
 
                 //loop to search for the element in the array
-                while ((item! = arr[mid]) && (lowerbound <= upperbound))
+                while (( item != arr[mid]) && (lowerbound <= upperbound))
                 {
                     if (item > arr[mid])
                         lowerbound = mid + 1;
@@ -83,7 +83,37 @@ namespace SearchAlgoritm
             }while ((ch == 'y') || (ch == 'Y'));
         }
 
-        
+        public void LinearSearch()
+        {
+            char ch;
+            //search for number of comparison
+            int ctr;
+            do
+            {
+                //accept the number to be searched 
+                Console.Write("\n enter the element you want to search:");
+                int item = Convert.ToInt32(Console.ReadLine());
+
+                ctr = 0;
+                for (i = 0; i < n; i++)
+                {
+                    ctr++;
+                    if (arr[i] == item)
+                    {
+                        Console.WriteLine("\n" + item.ToString() + "found in the array" + (i + 1).ToString());
+                        break;
+
+                    }
+                }
+                if (i == n)
+                    Console.WriteLine("\n" + item.ToString() + "not found in the array");
+                Console.WriteLine("\n Number of comparison:" + ctr);
+                Console.Write("\n Continue search (y/n):");
+                ch = Char.Parse(Console.ReadLine());
+            } while ((ch == 'y') || (ch == 'Y'));
+        }
+       
+        }
     }
 
 }
